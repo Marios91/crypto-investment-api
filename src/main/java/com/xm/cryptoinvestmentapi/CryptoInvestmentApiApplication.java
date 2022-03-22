7a4b68ec421c179d9d98e4953468db6705c88578
Marios <mkaralis@iknowhow.com>
2022-03-22 22:22:00 +0200

    add spring boot application

---
+++ src/main/java/com/xm/cryptoinvestmentapi/CryptoInvestmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.xm.cryptoinvestmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInvestmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoInvestmentApiApplication.class, args);
+	}
+
+}
