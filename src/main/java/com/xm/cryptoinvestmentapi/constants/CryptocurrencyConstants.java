30fd91d15c2df1592cec26eecfe73212554e2137
Marios <super_mario91@hotmail.com>
2022-04-15 14:14:42 +0300

    add cryptocurrency constant file

---
+++ src/main/java/com/xm/cryptoinvestmentapi/constants/CryptocurrencyConstants.java
@@ -0,0 +1,12 @@
+package com.xm.cryptoinvestmentapi.constants;
+
+public final class CryptocurrencyConstants {
+
+    public static final String COMMA_SEPARATOR = ",";
+    public static final String DOT = ".";
+
+    private CryptocurrencyConstants() {
+        throw new UnsupportedOperationException("We cannot call constructors on constant classes.");
+    }
+
+}
